1b3ffdb0163c37f14f9559143a44d107536fc888
Nate Stutte <ngs33@drexel.edu>
2020-12-17 22:08:04 -0500

    Fixed problem with scroll size

--- js/navbar-mobile.js
+++ js/navbar-mobile.js
@@ -5,14 +5,10 @@ $(document).ready(function() {
                 }else{
                     $("#navbar-button").attr("src", "imgs/open.png");
                 }
-<<<<<<< HEAD
                 if($("li").attr('navlink') == "true") {
                     $(".item_left").toggle("slow");
                     $(".item_right").toggle("slow");
                 }
-=======
-                $(".navbar li").toggle("slow");
->>>>>>> 9c67a0ded6f8367a35d046f1a48556abc15e70d4
             }
         )
     }
